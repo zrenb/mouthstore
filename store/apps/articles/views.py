c0f0c9d21545609c149f5960430e49a09e0e0b5c
wangsy <15892766524@163.com>
2019-04-15 17:07:19 +0800

    添加模板文件

--- store/apps/articles/views.py
+++ store/apps/articles/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.base import View
+
+
+class Articles(View):
+    def get(self, request):
+        return render(request, 'articles/index.html')
--- store/store/urls.py
+++ store/store/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 # from django.contrib import admin
 from django.views.static import serve
 
@@ -23,4 +23,6 @@ from store.settings import MEDIA_ROOT
 urlpatterns = [
     url(r'^admin/', xadmin.site.urls),
     url(r'^media/(?P<path>.*)', serve, {'document_root': MEDIA_ROOT}),
+    # 文章
+    url(r'^articles/', include('articles.urls', namespace="article"))
 ]
